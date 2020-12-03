e2a7badbe3ae4e1cb45b7d8055a1710197ba1f12
Icyimpaye Umutoni Marianne <61685113+BlessedMarianna@users.noreply.github.com>
2020-12-03 11:29:19 +0300

    Registered

---
+++ class-4/Profile.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("button").click(function(){
+    $("img").animate({
+      left: '250px',
+      height: '+=150px',
+      width: '+=150px',
+  $("Jobdescription").ready(function(){
+   $('div').fadeIn('slow');
+ });
+ $("Image").click(function(){
+     $("div").animate({left: '500px'});
+       '250px';
+       '+=150px';
+       '+=150px';
+   }); 
+  $("Hobbies").click(function(){
+    $("h1").animate({left: '500px'});
+  }); 
+});
+  });
